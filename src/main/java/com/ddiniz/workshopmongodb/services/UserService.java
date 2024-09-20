feb1e95f199fe8431edf1d3ca36674b6c541a36d
Derek Diniz <drk.diniz@gmail.com>
2024-09-20 13:19:53 -0300

    Inserção de usuário com POST

--- src/main/java/com/ddiniz/workshopmongodb/services/UserService.java
+++ src/main/java/com/ddiniz/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ddiniz.workshopmongodb.domain.User;
+import com.ddiniz.workshopmongodb.dto.UserDTO;
 import com.ddiniz.workshopmongodb.repository.UserRepository;
 import com.ddiniz.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,11 @@ public class UserService {
 		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 		}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 	
 }
